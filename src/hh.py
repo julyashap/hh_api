179d37fe11cf232bc5e6ff58894b3112a96da7d8
Julya Shapaeva <shapaeva03@mail.ru>
2024-04-15 16:48:49 +0300

    Realize abstract methods from APIVacancies in HH

--- src/hh.py
+++ src/hh.py
@@ -1,7 +1,23 @@
 from src.api_vacancies import APIVacancies
+import requests
 
 
 class HH(APIVacancies):
     """Класс для работы с API сайта hh.ru"""
 
-    pass
+    def __init__(self):
+        self.url = 'https://api.hh.ru/vacancies'
+        self.headers = {'User-Agent': 'HH-User-Agent'}
+        self.params = {'text': '', 'page': 0, 'per_page': 100}
+        self.vacancies = []
+
+    def load_vacancies(self, keyword: str) -> list:
+        self.params['text'] = keyword
+
+        while self.params.get('page') != 20:
+            response = requests.get(self.url, headers=self.headers, params=self.params)
+            vacancies = response.json()['items']
+            self.vacancies.extend(vacancies)
+            self.params['page'] += 1
+
+        return self.vacancies
