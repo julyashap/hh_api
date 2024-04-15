a5d22577dbed61ae6abf00c376bba6eca5896967
Julya Shapaeva <shapaeva03@mail.ru>
2024-04-15 16:48:29 +0300

    Add abstract methods to APIVacancies

--- src/api_vacancies.py
+++ src/api_vacancies.py
@@ -4,4 +4,10 @@ from abc import ABC, abstractmethod
 class APIVacancies(ABC):
     """Абстрактный класс для работы с API сайтов с вакансиями"""
 
-    pass
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def load_vacancies(self, keyword: str) -> list:
+        pass
