355e008ed0f9adfd39c67375fcd4c3d0d934c217
Julya Shapaeva <shapaeva03@mail.ru>
2024-04-15 20:04:23 +0300

    Add docstrings to methods

--- src/hh.py
+++ src/hh.py
@@ -6,12 +6,16 @@ class HH(APIVacancies):
     """Класс для работы с API сайта hh.ru"""
 
     def __init__(self):
+        """Конструктор класса HH"""
+
         self.url = 'https://api.hh.ru/vacancies'
         self.headers = {'User-Agent': 'HH-User-Agent'}
         self.params = {'text': '', 'page': 0, 'per_page': 100}
         self.vacancies = []
 
     def load_vacancies(self, keyword: str) -> list:
+        """Метод для получения списка вакансий по запросу"""
+
         self.params['text'] = keyword
 
         while self.params.get('page') != 20:
